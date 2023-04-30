14e9cd7179c2e11596f50cd2d273788a19dfee4b
prousium <121478654+prousium@users.noreply.github.com>
2023-04-30 14:28:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-function searchWeather() {
-  const apiKey = 'YOUR_API_KEY';
-  const searchInput = document.getElementById('search');
-  const location = document.querySelector('.location');
-  const temperature = document.querySelector('.temperature');
-  const weather = document.querySelector('.weather');
+const apiKey = "c0ef56da102aa15c8cc4fca6f3874ebc";
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${c0ef56da102aa15c8cc4fca6f3874ebc}`)
-    .then(response => response.json())
-    .then(data => {
-      location.textContent = data.name + ', ' + data.sys.country;
-      temperature.textContent = data.main.temp + ' °C';
-      weather.textContent = data.weather[0].description;
-    })
-    .catch(error => console.log(error));
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), {
+    origin: "cros",
+  });
+  const respData = await resp.json();
+
+  if (respData.cod === "404") {
+    // Show error message
+    main.innerHTML = `<div class="error">Please enter a valid location to see<br> the the Weather Details</div>`;
+  } else {
+    addWeatherToPage(respData);
+  }
 }
 
+function addWeatherToPage(data) {
+  const temp = Ktoc(data.main.temp);
+
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+
+  weather.innerHTML = `
+          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+          <small>${data.weather[0].main}</small>
+          
+          `;
+
+  //   cleanup
+  main.innerHTML = "";
+  main.appendChild(weather);
+}
+
+function Ktoc(K) {
+  return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
